e57d5ef0678cfcab6b171e28606d6d90269804bf
Aman Singh <amansinghs7010@gmail.com>
2023-07-29 19:06:15 -0400

    add password hashing

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services/password';
+
 // Interface that describes the properties
 // required to create a new User
 interface UserAttrs {
@@ -29,6 +31,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
