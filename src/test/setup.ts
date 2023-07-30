0802550ff699d36abec1883269a9a3fffed677a4
Aman Singh <amansinghs7010@gmail.com>
2023-07-30 15:19:34 -0400

    add tests for currentuser route

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,11 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+
+declare global {
+  var getAuthCookie: () => Promise<string[]>;
+}
 
 let mongo: any;
 
 beforeAll(async () => {
-  process.env.JWT_KEY = 'sadjasd';
+  process.env.JWT_KEY = 'somejwtsecret';
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
@@ -26,3 +32,17 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.getAuthCookie = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
