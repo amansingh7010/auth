4205dd520b05cfe34afc84df00478d3db20591e7
Aman Singh <amansinghs7010@gmail.com>
2023-07-30 11:55:08 -0400

    add authentication logic

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-import { Password } from '../services/password';
+import { PasswordManager } from '../services/password-manager';
 
 // Interface that describes the properties
 // required to create a new User
@@ -22,20 +22,32 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
-    const hashed = await Password.toHash(this.get('password'));
+    const hashed = await PasswordManager.toHash(this.get('password'));
     this.set('password', hashed);
   }
   done();
